c53cd0c2c1ee32217ff5538c906d28397be488d6
Ajay Thakur <blacklisted.web06@gmail.com>
2024-11-13 10:43:35 +0530

    latest changes

--- homepage.php
+++ homepage.php
@@ -3,8 +3,6 @@
 
 get_header(); 
 ?>
-    <!-- Link Swiper's CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 
  <!--  hero section -->
         <section class="hero-banner">
@@ -17,8 +15,8 @@ get_header();
                         <div class="content-box text-white">
                             <h1 class="banner-heading">Transform Your Visionary Ideas <br> Into Reality With Us</h1>
                             <div class="d-flex justify-content-center">
-                                <a href="#" class="btn btn-primary btn-custom me-2">Explore Our Firm</a>
-                                <a href="#" class="btn btn-secondary btn-custom">How We Work</a>
+                                <a href="#" class="btn btn-primary btn-custom me-4 rounded-0">Explore Our Firm</a>
+                                <a href="#" class="btn btn-secondary btn-custom rounded-0">How We Work</a>
                             </div>
                         </div>
                     </div>
@@ -29,16 +27,16 @@ get_header();
       <section class="common-section-spacing portfolio-section">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-8">
+                    <div class="col-md-10">
                         <div class="section-header">
-                            <span class="common-subheading">The Portfolio Of Stories </span>
+                            <span class="common-subheading primary-color text-uppercase">The Portfolio Of Stories </span>
                             <h2 class="commom-heading">Imagine Your Dream, <br>
                                 Beautifully Realised!</h2>
                             <p class="commom-description"> Let our innovative architecture and meticulous interior
                                 design inspire your next venture, giving form and structure to your dream space.</p>
                         </div>
                         <div class="portfolio-tabs">
-                            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                            <ul class="nav nav-pills mb-3 gap-3 mt-5" id="pills-tab" role="tablist">
                                 <li class="nav-item" role="presentation">
                                     <button class="nav-link active" id="pills-all-tab" data-bs-toggle="pill"
                                         data-bs-target="#pills-all" type="button" role="tab" aria-controls="pills-all"
@@ -83,73 +81,100 @@ get_header();
                     </div>
                 </div>
             </div>
-            <div class="container-fluid mt-5">
+            <div class="container-fluid mt-5 p-0">
                 <div class="tab-content" id="pills-tabContent">
                     <div class="tab-pane fade show active" id="pills-all" role="tabpanel"
                         aria-labelledby="pills-all-tab">
-                        <div class="swiper mySwiper">
+                        <div class="swiper portfolio-slider">
                             <div class="swiper-wrapper">
                                 <div class="swiper-slide">
-                                    <div class="position-relative">
+                                    <div class="portfolio-card position-relative">
                                         <img src="<?php echo site_url(); ?>/wp-content/uploads/2024/11/portfolio-img1.webp" class="img-fluid"
-                                            alt="portfolio-img1">
-                                        <div
-                                            class="overlay position-absolute top-0 start-0 w-100 h-100 bg-dark bg-opacity-50 d-flex justify-content-center align-items-center text-white opacity-0 hover-opacity-100 transition-all">
-                                            <span class="fs-4">
-                                                <img src="Assets/images/location-icon.png" alt="Location" class="me-2"
-                                                    style="width: 20px; height: 20px;">Chandigarh
-                                            </span>
+                                            alt="portfolio-img1">                                      
+                                        <div class="overlay-text text-white">
+                                            <h3>Sukhawas</h3>
+                                            <h4>IDEATION</h4>
+                                            <p>Showcasing a modern house interior that blends sleek design with functionality.</p>
+                                             <p>Residential</p>
+                                             <p>Chandigarh</p>
+                                             <a href="javascript:void(0)" class="view project details-link">
+                                                view project details</a>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="swiper-slide">
-                                    <div class="position-relative">
+                                    <div class="portfolio-card position-relative">
                                         <img src="<?php echo site_url(); ?>/wp-content/uploads/2024/11/portfolio-img2.webp" class="img-fluid"
                                             alt="portfolio-img2">
-                                        <div
-                                            class="overlay position-absolute top-0 start-0 w-100 h-100 bg-dark bg-opacity-50 d-flex justify-content-center align-items-center text-white opacity-0 hover-opacity-100 transition-all">
-                                            <span class="fs-4">Overlay 2</span>
+                                         <div class="overlay-text text-white">
+                                            <h3>MODERN HOUSE INTERIORS</h3>
+                                            <h4>IDEATION</h4>
+                                            <p>Infusing practical aesthetics into the interior profile of the house.</p>
+                                             <p>Commercial</p>
+                                             <p>Gurugram</p>
+                                             <a href="javascript:void(0)" class="view project details-link">
+                                                view project details</a>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="swiper-slide">
-                                    <div class="position-relative">
+                                    <div class="portfolio-card position-relative">
                                         <img src="<?php echo site_url(); ?>/wp-content/uploads/2024/11/portfolio-img3.webp" class="img-fluid"
                                             alt="portfolio-img3">
-                                        <div
-                                            class="overlay position-absolute top-0 start-0 w-100 h-100 bg-dark bg-opacity-50 d-flex justify-content-center align-items-center text-white opacity-0 hover-opacity-100 transition-all">
-                                            <span class="fs-4">Overlay 3</span>
-                                        </div>
+                                       <div class="overlay-text text-white">
+											<h3>RAEIN BASERA</h3>
+											<h4>IDEATION</h4>
+											<p>Creating sustainable and energy-efficient structures within the existing structure.</p>
+											<p>Commercial</p>
+											<p>Gurugram</p>
+											<a href="javascript:void(0)" class="view project details-link">
+												view project details</a>
+										</div>
                                     </div>
                                 </div>
                                 <div class="swiper-slide">
-                                    <div class="position-relative">
+                                    <div class="portfolio-card position-relative">
                                         <img src="<?php echo site_url(); ?>/wp-content/uploads/2024/11/portfolio-img4.webp" class="img-fluid"
                                             alt="portfolio-img4">
-                                        <div
-                                            class="overlay position-absolute top-0 start-0 w-100 h-100 bg-dark bg-opacity-50 d-flex justify-content-center align-items-center text-white opacity-0 hover-opacity-100 transition-all">
-                                            <span class="fs-4">Overlay 4</span>
-                                        </div>
+										<div class="overlay-text text-white">
+											<h3>Anandlay</h3>
+											<h4>IDEATION</h4>
+											<p>Reform and revolutionise the design language and architecture of this residential space.</p>
+											<p>Residential</p>
+											<p>Chandigarh</p>
+											<a href="javascript:void(0)" class="view project details-link">
+												view project details</a>
+										</div>
                                     </div>
                                 </div>
                                 <div class="swiper-slide">
-                                    <div class="position-relative">
+                                    <div class="portfolio-card position-relative">
                                         <img src="<?php echo site_url(); ?>/wp-content/uploads/2024/11/portfolio-img4.webp" class="img-fluid"
                                             alt="portfolio-img4">
-                                        <div
-                                            class="overlay position-absolute top-0 start-0 w-100 h-100 bg-dark bg-opacity-50 d-flex justify-content-center align-items-center text-white opacity-0 hover-opacity-100 transition-all">
-                                            <span class="fs-4">Overlay 4</span>
-                                        </div>
+                                     <div class="overlay-text text-white">
+											<h3>RAEIN BASERA</h3>
+											<h4>IDEATION</h4>
+											<p>Creating sustainable and energy-efficient structures within the existing structure.</p>
+											<p>Commercial</p>
+											<p>Gurugram</p>
+											<a href="javascript:void(0)" class="view project details-link">
+												view project details</a>
+										</div>
                                     </div>
                                 </div>
                                 <div class="swiper-slide">
-                                    <div class="position-relative">
+                                    <div class="portfolio-card position-relative">
                                         <img src="<?php echo site_url(); ?>/wp-content/uploads/2024/11/portfolio-img3.webp" class="img-fluid"
                                             alt="portfolio-img4">
-                                        <div
-                                            class="overlay position-absolute top-0 start-0 w-100 h-100 bg-dark bg-opacity-50 d-flex justify-content-center align-items-center text-white opacity-0 hover-opacity-100 transition-all">
-                                            <span class="fs-4">Overlay 4</span>
-                                        </div>
+                                       <div class="overlay-text text-white">
+											<h3>RAEIN BASERA</h3>
+											<h4>IDEATION</h4>
+											<p>Creating sustainable and energy-efficient structures within the existing structure.</p>
+											<p>Commercial</p>
+											<p>Gurugram</p>
+											<a href="javascript:void(0)" class="view project details-link">
+												view project details</a>
+										</div>
                                     </div>
                                 </div>
                             </div>
@@ -167,22 +192,29 @@ get_header();
                         aria-labelledby="pills-commercial-tab">e...</div>
                 </div>
             </div>
+			  <div class="container">
+					<div class="text-center pt-4">
+						<a href="javascript:void(0)" class="btn btn-outline-primary rounded-0"> Show Me More Projects</a>
+					</div>
+				</div>
         </section>
 
- <!-- Swiper JS -->
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-
-    <!-- Initialize Swiper -->
-    <script>
-        var swiper = new Swiper(".mySwiper", {
-            slidesPerView: 4,
-            spaceBetween: 0,
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-            },
-        });
-    </script>
+        <!-- Reach Out section style SH -->
+        <section class="common-section-spacing react-out-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10">
+                        <div class="section-header">
+                            <span class="common-subheading primary-color text-uppercase">Reach Out to us!
+                            </span>
+                            <h2 class="commom-heading text-white">Lets Discuss Your Project!</h2>
+                            <p class="commom-description text-white"> Bring your design requirements to the table and we will make sure to breathe life into them, both visually and structurally.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Reach Out section style EH -->
 
 
 <?php get_footer(); ?>
